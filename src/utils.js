2c969de38177da6e4bcd072f32f3fb3a87838c09
Qihang Zeng <qz689@nyu.edu>
2019-03-04 20:48:47 -0500

    adding to-do functions

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,28 @@
     for unit testing
 */
 
+/*brief:
+Here is a list of functions to write for
+unit-testing:
+    Basic:
+    1. findMax: return max in an array
+    2. findMin: return min in an array
+    3. filter(array, string): filter out
+        a string in a list
+        return a copy
+    4. tolowercase
+    5. touppercase
+    6. text parsing
+    7. find() find an item in an array
+    
+    High Order Functions:
+    1. bundle 2 functions:
+    2. ...
+
+*/
+
+
+
 /* write a function that returns the maximum
 of an array. Return nan if argument is not an 
 array*/
@@ -22,6 +44,10 @@ function findMax(l) {
     }
 }
 
+
+
+
+//add functions to exports
 module.exports = {
     findMax:findMax
 };
