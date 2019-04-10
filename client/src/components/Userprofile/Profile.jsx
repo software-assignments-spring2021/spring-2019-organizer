103c8727ca375b1508708dbc2f084cb7170a7761
Qihang Zeng <qz689@nyu.edu>
2019-04-10 01:21:54 -0400

    leaving out unused library

--- client/src/components/Userprofile/Profile.jsx
+++ client/src/components/Userprofile/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Radium from 'radium';
 import { Nametab, Majortab} from './Profiletab';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types'; leave for later
 
 //base sytle for the user profile div
 const profileStyle = {
