10b4c438e34d1de2818e732d1416697043d9939e
Angela Luo <al4951@nyu.edu>
2019-04-14 15:34:03 -0400

    remove redundant codes

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -8,9 +8,15 @@ class Schedule extends Component {
     super();
     this.state = {
       schedules: {
+<<<<<<< HEAD
       '2019-05-10' : [{ class: 'Agile Software Development', name: 'HW10', duetime: '2019-05-10T08:00', estimated: 1.5, tag: ['homework'], difficulty: 2},
       { class: 'Machine Learning', name: 'Homework 3', duetime: '2019-05-10T08:00', estimated: 8, tag: ['homework', 'haha'], difficulty: 5}],
       '2019-05-15': [{ class: 'SSPC', name: 'Final Paper', duetime: '2019-05-15T15:30', estimated: 5, tag: ['homework'], difficulty: 4}]
+=======
+      '04/18/2019' : [{ subject: 'Agile Software Development', name: 'HW10', date: '04/18/2019', estimated: 1.5, type: 'homework', text: '' }],
+      '05/02/2019': [{ subject: 'Machine Learning', name: 'Homework 3', date: '05/02/2019', estimated: 8, type: 'homework', text: '' }],
+      '01/01/2020': [{ subject: 'SSPC', name: 'Final Paper', date: '01/01/2020', estimated: 5, type: 'homework', text: '' }]
+>>>>>>> remove redundant codes
       }
     };
   }
