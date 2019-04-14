c7455652178b7650fbd1f8dae38ed566b4863f51
Junno Yau <jq488>
2019-04-14 17:04:07 -0400

    updated login
    -validated log in button
    -made the interface look nicer
    -redirected to Schedules page if the password and username matches

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Sidebar from './components/Sidebar';
 import Schedule from './components/Schedule';
 import Profile from './components/Userprofile/Profile';
-import Login from './components/Login'
+// import Login from './components/Login'
+import Loginpage from './components/Loginpage'
 
 import './App.css';
+
 
 //const testprofile = 'https://raw.githubusercontent.com/nyu-software-engineering/organizer/master/documentation/web_mockup_Mark/home_page.png';
 
@@ -20,7 +22,8 @@ class App extends Component {
           <Route path="/schedules" component={Schedule} />
           <Route path="/tags" />
           <Route path="/analysis" />
-          <Route path="/login" component={Login} />
+          <Route path="/login" component={Loginpage} 
+          />
         </div>
       </Router>
     );
