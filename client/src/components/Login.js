b9669ed7febf5830637a676b05af7944daf456a8
Junno Yau <jq488>
2019-04-14 17:04:07 -0400

    updated login
    -validated log in button
    -made the interface look nicer
    -redirected to Schedules page if the password and username matches

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,74 +1,36 @@
 import React, { Component } from 'react';
 import { Card, Row, Col} from 'react-bootstrap';
+
 import '../css/Login.css';
 
-const UNAME = '123456';
-const PWD = '123456';
 
 class Login extends Component{
-    constructor(){
-        super();
-        this.state=({
-          userName:'123456',
-          pwd:'123456'
-        })
-      }
-    
-      handleUserNameChange = (e) =>{
-        this.setState({
-          userName:e.target.value
-        })
-      }
-    
-      handlePwdChange = (e) =>{
-        this.setState({
-          pwd:e.target.value
-        })
-      }
-    
-      handleLogin = () =>{
-        if(this.state.userName===UNAME&&this.state.pwd===PWD){
-          alert("Login successfully!");
-        }
-        else if(this.state.userName!==UNAME){
-          alert("Fail to log in:Username is wrong!");
-        }
-        else if(this.state.pwd!==PWD){
-          alert("Fail to log in:Passwoed is wrong! ");
-        }
-        else{
-          alert("Fail to log!");
-        }
-      }
-    
-      handleSign = () =>{
-        alert("Sign in successfully!");
-      }
-    
 
     render(){
         return(
             
-            <Card.Title id ="login">
-                <Card.Header>Welcome to Log in</Card.Header>
+            <div id ="login">
+                <Col className="titles1"> NYU Classes Organizer</Col>
+                <Col className="titles1"> Please Login to NYU Classes</Col>
                 <Card.Body className="log-box">
                     <Card.Title>
-                        <Col className="text">Username:</Col>
+                      <Row>                        
+                        <Col className="text">NetID:</Col>     
                         <Col><input type="text" className="uname" onChange={this.props.handleUserNameChange}/></Col>
-                    </Card.Title>
-                    <Card.Title>
-                        <Row>
                         <Col className="text">Password:</Col>
                         <Col><input type="password" className="pwd" onChange={this.props.handlePwdChange}/></Col>
-                        </Row>
+                      </Row>
                     </Card.Title>
+                    
                     <Col>
                     <button className="btn btn-primary log-btn" onClick={this.props.handleLogin}>Login</button>
-                    <button className="btn btn-primary sign" onClick={this.props.handleSign}>sign in with Email</button>
+                    <button className="btn btn-primary sign" onClick={this.props.handleSign}>Sign in with Email</button>
                     </Col>
+ 
                 
                 </Card.Body>
-            </Card.Title>
+              
+            </div>
         );
     }
 
@@ -76,3 +38,8 @@ class Login extends Component{
 }
 
 export default Login;
+
+// handleUserNameChange={this.handleUserNameChange}
+// handlePwdChange={this.handlePwdChange}
+// handleSign={this.handleSign}
+// handleLogin={this.handleLogin}
