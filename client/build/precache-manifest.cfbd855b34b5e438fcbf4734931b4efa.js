40de100829c41c2f83ece3f9e1af817db883e82f
Zerichen <cz1296@nyu.edu>
2019-05-09 05:02:51 -0400

    fixing bugs

---
+++ client/build/precache-manifest.cfbd855b34b5e438fcbf4734931b4efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6fe489de02bdd6b8d86",
+    "url": "/static/css/main.1800d839.chunk.css"
+  },
+  {
+    "revision": "a6fe489de02bdd6b8d86",
+    "url": "/static/js/main.c2559db5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9a4fcf503c7f55f03fb",
+    "url": "/static/css/2.0fd09e22.chunk.css"
+  },
+  {
+    "revision": "d9a4fcf503c7f55f03fb",
+    "url": "/static/js/2.ea518b71.chunk.js"
+  },
+  {
+    "revision": "4fc3b015b1621cb2cfc442c3eb96b364",
+    "url": "/index.html"
+  }
+];
