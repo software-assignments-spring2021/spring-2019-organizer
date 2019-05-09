e32fc3bb9c3912446d58959ea72e6b1febf68b6d
Zerichen <cz1296@nyu.edu>
2019-05-09 05:07:38 -0400

    deleting redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Sidebar from './components/Sidebar';
 import Schedule from './components/Schedule';
 import Settingpage from './components/Settingpage';
